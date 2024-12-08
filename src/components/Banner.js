b7b328f55c4015e2dc45a6f03eb43ab9693a5d48
Manvi Dewangan <109518835+Manvi1902@users.noreply.github.com>
2024-12-08 21:56:39 +0530

    Update Banner.js

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -71,9 +71,7 @@ export const Banner = () => {
                   I enjoy solving complex real-world problems creatively and Ready to enhance 
                   my skills while contributing to innovative projects that make a remarkable impact.
                  </p>
-                      {/* <a href='https://drive.google.com/file/d/1W4nHXTjL_IhZ0da8evp-QCDeeEcnLrfj/view?usp=sharing'>
-                      <button className="resume-link"><span>My Resume</span></button>*/}
-                  </a>
+                     
               </div>}
             </TrackVisibility>
         </Col>
